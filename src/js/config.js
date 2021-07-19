f9f6d27f00264ea4f0d7db915aa1aaf0d1f3009e
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2021-07-19 10:50:17 -0700

    Created Forkify

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '7f6baf1a-2652-4072-9eca-a4a830b1d2c7';
+export const MODAL_CLOSE_SEC = 2.5;
